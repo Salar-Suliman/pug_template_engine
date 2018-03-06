3cf677c8d2b128f9cf721349d49e0584e23ae325
Salar <ceger1985@gmail.com>
2018-03-06 14:31:01 +0100

    install webpack, pug-loader and gup

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+
+var config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: '!!pug-loader!src/index.pug',
+    }),
+  ]
+}
+module.exports = config;
